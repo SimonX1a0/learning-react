2eb915c78239c33b8f907c7893663ff31d0927db
SimonX1ao <simonxiao81@gmail.com>
2025-11-01 22:58:56 -0400

    added MVC to the backend and connected to the database

--- server/index.js
+++ server/index.js
@@ -1,10 +1,20 @@
 import express from 'express'
+import mongoose from 'mongoose'
 
 const app = express();
 const PORT = 5000;
+const MONGO_URI = "mongodb://localhost:27017/myDatabase";
+
+try {
+    mongoose.connect(MONGO_URI);
+    console.log("connected to mongoDB");
+} catch (error) {
+    console.error("failed connecting to mongoDB:", error);
+}
+
 
 app.get("/", (req, res)=>{
-    res.sendFile("../src/main.jsx");
+
 });
 
 app.listen(PORT, () => {
